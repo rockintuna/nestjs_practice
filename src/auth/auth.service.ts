65abb147f88ca705b10b6edb7283c823129beced
rockintuna <rockintuna@naver.com>
2025-09-24 23:24:21 +0900

    feat: AuthService에 JWT 생성 기능 추가 및 jwtConstants 파일 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,23 @@
 import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     // UsersService is imported to validate user credentials
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
 
     async signIn(username: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(username);
-        if (user == null) {
-            throw new NotFoundException('User not found');
-        }
         if (user?.password !== pass) {
             throw new UnauthorizedException();
         }
-        const { password, ...result } = user;
-        // TODO: Generate a JWT and return it here
-        // instead of the user object
-        return result;
+        const payload = { sub: user.userId, username: user.username };
+        return { 
+            access_token: this.jwtService.sign(payload) 
+        };
     }
 }
