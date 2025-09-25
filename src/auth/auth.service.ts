cc41d745e9ba5afd42590dde725595ac97066b6a
rockintuna <rockintuna@naver.com>
2025-09-25 23:00:22 +0900

    feat: AuthService에서 불필요한 예외 임포트 제거 및 launch.json 파일 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  UnauthorizedException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
